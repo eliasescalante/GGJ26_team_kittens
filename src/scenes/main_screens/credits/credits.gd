25f893a7c7f905847b6a4ba5f871edfd3345ca45
Matías Valentín Muñoz Espinoza <writkas2@gmail.com>
2026-01-28 17:54:19 -0300

    credits

---
+++ src/scenes/main_screens/credits/credits.gd
@@ -0,0 +1,6 @@
+extends Control
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://src/scenes/main_screens/main_menu/main_menu.tscn")
+	
--- src/scenes/main_screens/main_menu/main_menu.gd
+++ src/scenes/main_screens/main_menu/main_menu.gd
@@ -4,3 +4,7 @@ extends Control
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://src/scenes/cinematic/cinematic.tscn")
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://src/scenes/main_screens/credits/credits.tscn")

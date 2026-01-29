5bf66382d4daa760811c1cf221389e245456e674
Matías Valentín Muñoz Espinoza <writkas2@gmail.com>
2026-01-29 16:33:13 -0300

    Se añade link de mi canal de youtube

--- src/scenes/main_screens/credits/credits.gd
+++ src/scenes/main_screens/credits/credits.gd
@@ -4,3 +4,7 @@ extends Control
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://src/scenes/main_screens/main_menu/main_menu.tscn")
 	
+
+
+func _on_credits_meta_clicked(meta: Variant) -> void:
+	OS.shell_open(meta)

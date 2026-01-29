71e44b092dc292fb7f6839f1eaf5fd46a8d19f3c
Valentinna1 <valenperez01@hotmail.com>
2026-01-29 19:33:05 -0300

    agrego exit

--- src/scenes/main_screens/main_menu/main_menu.gd
+++ src/scenes/main_screens/main_menu/main_menu.gd
@@ -8,3 +8,8 @@ func _on_start_pressed() -> void:
 
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://src/scenes/main_screens/credits/credits.tscn")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+	pass # Replace with function body.

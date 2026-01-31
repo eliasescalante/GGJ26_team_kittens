c6cfe8270bb7a40f972c3d0e8001f77a99c896a3
Valentinna1 <valenperez01@hotmail.com>
2026-01-30 22:18:00 -0300

    animaciones al player

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -30,26 +30,36 @@ func _physics_process(delta):
 
 func update_animation(dir_x):
 	var vida = GameManager.player_lives
-	var current_anim = "walk_right"
+	var current_anim = ""
 	
-	if vida == 3:
-		current_anim = "walk_right_d_1"
-	if vida == 2:
-		current_anim = "walk_right_d_2"
-	if vida == 1:
-		current_anim = "walk_right_d_3"
-		
-	if dir_x == 0:
-		if velocity.y == 0:
-			anim.play("idle")
-		else:
-			anim.play(current_anim)
+	#idle
+	if dir_x == 0 and velocity.y == 0:
+		if vida == 4:
+			current_anim = "idle"
+		if vida == 3:
+			current_anim = "idle_1"
+		if vida == 2:
+			current_anim = "idle_2"
+		if vida == 1:
+			current_anim = "idle_3"
 	else:
+		if vida == 4:
+			current_anim = "walk_right"
+		if vida == 3:
+			current_anim = "walk_right_d_1"
+		if vida == 2:
+			current_anim = "walk_right_d_2"
+		if vida == 1:
+			current_anim = "walk_right_d_3"
+	
+	#reproducir y flip
+	if current_anim != "":
 		anim.play(current_anim)
-		if dir_x < 0:
-			anim.flip_h = true
-		else:
-			anim.flip_h = false
+	
+	if dir_x < 0:
+		anim.flip_h = true
+	if dir_x > 0:
+		anim.flip_h = false
 
 func take_hit(amount):
 	GameManager.lose_life()
